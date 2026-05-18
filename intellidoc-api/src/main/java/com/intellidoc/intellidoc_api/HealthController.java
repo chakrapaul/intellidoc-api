384a67a960c773d41d49c3c6a7211672982ef4e4
chakrapaul <cpaka@charlotte.edu>
2026-05-18 19:02:09 -0400

    Day 1:Initial setup with health endpoint

---
+++ intellidoc-api/src/main/java/com/intellidoc/intellidoc_api/HealthController.java
@@ -0,0 +1,20 @@
+package com.intellidoc.intellidoc_api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class HealthController {
+    @GetMapping("/health")
+    public Map<String,String> health(){
+        return Map.of(
+                "status", "UP",
+                "service", "IntelliDoc API",
+                "version", "0.1.0"
+        );
+    }
+}
